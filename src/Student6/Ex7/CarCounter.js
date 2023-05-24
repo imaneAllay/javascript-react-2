bf4fb3245e6428f3740895c0e9173b3d378e0811
Imane Allay <iallay@xpanxion.com>
2023-05-24 16:41:27 -0400

    Add Ex6

--- src/Student6/Ex7/CarCounter.js
+++ src/Student6/Ex7/CarCounter.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
  
 export const CarCounter =()=>{
     let [countF,setCountF]=useState(0)
     let [countT,setCountT]=useState(0)
     let [countC,setCountC]=useState(0);
 
-    const handleClick =(carType){
-        if (carType=="Ford"){
-            setCountF(countF+1) 
-        }
-    }
+    const handleFord =()=>setCountF(countF+1)
+    const handToyota =()=>setCountT(countT+1)
+    const handleChevy =()=>setCountC(countC+1)
+
+        
     return(
         <div>
-        <button>Ford</button>
-        <button>Toyota</button>
-        <button>Chevy</button>
-        Ford:
-        Toyota:
-        Chevy:
-        ToTal:
+        <button onClick={()=>handleFord()}>Ford</button>
+        <button onClick={()=>handToyota()}>Toyota</button>
+        <button onClick={()=>handleChevy()}>Chevy</button>
+        <br/>
+        Ford:{countF}
+        <br/>
+        Toyota:{countT}
+        <br/>
+        Chevy:{countC}
+        <br/>
+        ToTal:{countF+countT+countC}
         </div>
     )
 }
