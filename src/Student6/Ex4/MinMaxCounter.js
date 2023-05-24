284079e3665073e29b4ad9a6cbdbb1f067020278
Imane Allay <iallay@xpanxion.com>
2023-05-24 12:31:59 -0400

    Add Ex4

---
+++ src/Student6/Ex4/MinMaxCounter.js
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+
+
+export const MinMaxCounter  =()=>{
+    let [counter,setCounter]=useState(0)
+    let [max,setMax]=useState(0)
+    let [min,setMin]=useState(0)
+
+    const onhandleMax =()=>{
+       let currentValue = counter+1
+       setCounter(currentValue);
+        if (currentValue>max) {
+            setMax(currentValue)
+        }   
+    }
+
+    const onhandleMin =()=>{
+        let currentValue = counter--
+        setCounter(currentValue);
+         if (currentValue<min) {
+             setMin(currentValue)
+         }   
+     }
+     
+    
+    return(
+        <div>
+            <button onClick={()=>onhandleMax()} >Up</button>
+            {counter}
+            <button onClick={()=>onhandleMin()}>Down</button>
+            <p>Min: {min}</p>
+            <p>Max:{max}</p>
+        </div>
+    )
+}
+//update Ex4
